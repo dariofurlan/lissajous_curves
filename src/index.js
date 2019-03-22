552fa0d0131b39a20834b26b8fe40376f82cd2c3
Dario Furlan <dario.furlan.51@gmail.com>
2019-03-22 18:03:55 +0100

    backup-push

--- src/index.js
+++ src/index.js
@@ -59,11 +59,11 @@ class TableData {
         this.edge = edge;
     }
 
-    draw(ctx, TEST) {
+    draw(ctx, rad) {
         // ctx.strokeRect(this.x, this.y, this.edge, this.edge);
         if (!this.circle)
             return;
-        this.circle.draw(ctx, TEST);
+        this.circle.draw(ctx, rad);
     }
 
     setCircle(factor) {
@@ -105,9 +105,9 @@ class Table {
         }
     }
 
-    draw(ctx, TEST) {
+    draw(ctx, rad) {
         // ctx.strokeRect(this.x, this.y, this.padded_size, this.padded_size);
-        this.grid.forEach(row => row.forEach(data => data.draw(ctx, TEST)));
+        this.grid.forEach(row => row.forEach(data => data.draw(ctx, rad)));
 
         let trails_loop = () => {
             this.reduced = true;
@@ -115,10 +115,9 @@ class Table {
                 for (let x = 1; x < this.trails[y].length; x++) {
                     let trail = this.trails[y][x];
 
-                    // trail.forEach((value, index, array) => array.forEach(()))
                     console.log("before: " + trail.length);
                     console.log(JSON.stringify(trail));
-                    for (let i = 0; i < trail.length-1; i++) {
+                    for (let i = 0; i < trail.length - 1; i++) {
                         for (let j = i + 1; j < trail.length; j++) {
                             if (trail[j][0] === trail[i][0] && trail[j][1] === trail[i][1]) {
                                 // console.log("duplicated: "+JSON.stringify([trail[j],trail[i]],null, 4));
@@ -134,7 +133,7 @@ class Table {
             // setTimeout(trails_loop, 2000)
         };
 
-        if (this.trails[1][1].length >= 500 || this.reduced) {
+        if (this.trails[1][1].length >= 750 || this.reduced) {
             for (let y = 1; y <= this.num_circles; y++) {
                 for (let x = 1; x <= this.num_circles; x++) {
                     let c_x = this.grid[0][x].circle._x;
@@ -167,7 +166,7 @@ class Table {
                     ctx.stroke();
                 }
             }
-            if (this.trails[1][1].length === 499) setTimeout(trails_loop);
+            if (this.trails[1][1].length === 749) setTimeout(trails_loop);
         }
         //throw new Error("AAA");
     }
@@ -200,15 +199,36 @@ function Animation(factors) {
         };
 
         this.animation_stop = document.getElementById('animation_stop');
-        this.running = true;
+        this.running = false;
+        this.animation_stop.innerText = "START";
         this.animation_stop.onclick = () => {
-            if (this.running)
+            if (this.running) {
                 this.running = false;
-            else {
+                this.animation_stop.innerText = "START";
+            } else {
                 this.running = true;
                 this.start();
+                this.animation_stop.innerText = "PAUSE";
             }
         };
+
+        this.animation_forward = document.getElementById('animation_forward');
+        this.animation_forward.innerHTML = ">";
+        this.animation_forward.onclick = () => {
+            animation_step();
+        };
+
+        this.animation_backward = document.getElementById('animation_backward');
+        this.animation_backward.innerText = "<";
+        this.animation_backward.onclick = () => {
+            animation_backward();
+        };
+
+        this.radians_indicator = document.getElementById('radians_indicator');
+        this.radians_indicator.innerHTML = "0&pi;";
+
+        this.steps_indicator = document.getElementById('steps_indicator');
+        this.steps_indicator.innerText = "";
     };
     settings();
 
@@ -235,15 +255,29 @@ function Animation(factors) {
     let sec = 0;
     let fps = 0;
     let rad = 0;
+    let steps_counter = 0;
     const N_STEPS = 80;
     let step = 2 * Math.PI / N_STEPS;
 
-    let animation_loop = (timestamp) => {
+    let animation_step = (timestamp) => {
+        calc_radians(rad);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         table.draw(ctx, rad);
-        frames++;
+        steps_counter++;
         rad += step;
-        let now = Math.floor(timestamp / 1000);
+    };
+
+    let animation_backward = () => {
+        rad -= step;
+        steps_counter--;
+        calc_radians(rad);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        table.draw(ctx, rad);
+    };
+
+    let animation_loop = (timestamp) => {
+        animation_step();
+        frames++;
         if (Animation.drawFPS) {
             ctx.font = '1.2em Courier New';
             if (fps <= 50)
@@ -255,6 +289,7 @@ function Animation(factors) {
             ctx.fillText(fps, 5, 20);
             ctx.fillStyle = "black";
         }
+        let now = Math.floor(timestamp / 1000);
         if (now !== sec) {
             console.log("sec: " + sec + " fps: " + frames);
             fps = frames;
@@ -264,6 +299,18 @@ function Animation(factors) {
         if (this.running)
             window.requestAnimationFrame(animation_loop);
     };
+    let calc_radians = (r) => {
+        let pi = Math.floor(r / Math.PI);
+        let pi_half = Math.floor((r - Math.PI * pi) / (Math.PI / 2));
+        let pi_third = Math.floor((r - Math.PI * pi) / (Math.PI / 3));
+        let pi_fourth = Math.floor((r - Math.PI * pi) / (Math.PI / 4));
+        let pi_sixth = Math.floor((r - Math.PI * pi) / (Math.PI / 3));
+        let pi_tenth = Math.floor((r - Math.PI * pi) / (Math.PI / 10));
+        const simbol = "&pi;";
+        this.radians_indicator.innerHTML = pi + simbol + " " + pi_tenth + "/" + 10 + simbol;
+        this.steps_indicator.innerText = steps_counter;
+    };
+
     let trails_loop = () => {
 
         for (let y = 1; y < table.trails.length; y++) {
@@ -293,4 +340,5 @@ function Animation(factors) {
 }
 
 // TODO alongside animation do a process that every second or prefixed period, cleans the path generated by te lines
-new Animation([1, 1.2, 1.5,2]).start();
+// TODO  the trails loop with this approach is just a bodge, calculate the length of each trail and then stop pushing after that
+new Animation([1, 2, 3, 4]);
